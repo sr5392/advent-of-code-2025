2eb8738c74c4677acd78ea5ed1119ef11f689667
sr5392 <61883265+sr5392@users.noreply.github.com>
2025-12-02 00:24:24 +0100

    Add day 1 part 2

--- aoc_01/main.c
+++ aoc_01/main.c
@@ -41,11 +41,64 @@ int get_password(void) {
     return password;
 }
 
+int get_password_2(void) {
+    FILE* fp = fopen("./input.txt", "r");
+    if (!fp) {
+        perror("Unable to open file");
+        exit(EXIT_FAILURE);
+    }
+    int dial = 50;
+    int password = 0;
+    while (true) {
+        char rotation_direction = '\0';
+        int rotation_distance = 0;
+        const int arg_count = fscanf(fp, "%c%d\r\n", &rotation_direction, &rotation_distance);
+        if (arg_count == EOF)
+            break;
+        if (arg_count != 2) {
+            fclose(fp);
+            printf("Invalid file format");
+            exit(EXIT_FAILURE);
+        }
+        switch (rotation_direction) {
+            case 'L':
+                if (dial == 0)
+                    password--;
+                dial -= rotation_distance;
+                while (dial < 0) {
+                    dial += 100;
+                    ++password;
+                }
+                if (dial == 0)
+                    ++password;
+                break;
+            case 'R':
+                dial += rotation_distance;
+                while (dial > 99) {
+                    dial -= 100;
+                    ++password;
+                }
+                break;
+            default:
+                fclose(fp);
+                printf("Invalid file format");
+                exit(EXIT_FAILURE);
+        }
+    }
+    fclose(fp);
+    return password;
+}
+
 int part_1() {
     return get_password();
 }
 
+int part_2() {
+    return get_password_2();
+}
+
 int main(void) {
     printf("Part One: %d\n", part_1());
-    return 0;
+    printf("Part Two: %d\n", part_2());
+    return EXIT_SUCCESS;
 }

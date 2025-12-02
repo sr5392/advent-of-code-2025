e9877077a832bbb704ce99e5717afffff98390ae
sr5392 <61883265+sr5392@users.noreply.github.com>
2025-12-02 23:07:15 +0100

    Add day 2 part 2

--- aoc_02/main.c
+++ aoc_02/main.c
@@ -20,7 +20,39 @@ bool is_valid_id(const unsigned long id) {
     return false;
 }
 
-unsigned long sum_invalid_ids(void) {
+bool is_valid_id_2(const unsigned long id) {
+    char buffer[16] = {0};
+    snprintf(buffer, sizeof(buffer), "%lu", id);
+    const size_t id_length = strlen(buffer);
+    if (id_length < 2)
+        return true;
+    char buffer_search[16] = {0};
+    size_t search_length = 0;
+    bool invalid = false;
+    for (size_t i = 0; i < (id_length / 2); ++i) {
+        strncpy(buffer_search, buffer, i + 1);
+        search_length = i + 1;
+        if (id_length % search_length != 0) {
+            continue;
+        }
+        size_t k = 0;
+        bool match = true;
+        for (size_t j = 0; j < id_length; ++j) {
+            if (buffer[j] != buffer_search[k]) {
+                match = false;
+                break;
+            }
+            k = (k + 1) % search_length;
+        }
+        if (match) {
+            invalid = match;
+            break;
+        }
+    }
+    return !invalid;
+}
+
+unsigned long sum_invalid_ids(bool (*is_valid_id)(unsigned long)) {
     FILE* fp = fopen("./input.txt", "r");
     if (!fp) {
         exit(EXIT_FAILURE);
@@ -46,10 +78,15 @@ unsigned long sum_invalid_ids(void) {
 }
 
 unsigned long part_1(void) {
-    return sum_invalid_ids();
+    return sum_invalid_ids(is_valid_id);
+}
+
+unsigned long part_2(void) {
+    return sum_invalid_ids(is_valid_id_2);
 }
 
 int main(void) {
     printf("Sum of invalid IDs: %lu\n", part_1());
-    return 0;
+    printf("Sum of invalid IDs: %lu\n", part_2());
+    return EXIT_SUCCESS;
 }
